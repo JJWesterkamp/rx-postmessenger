b42243c7ccc12c3c6d210e426d1d6c5b6dc2efa3
Jeffrey Westerkamp <jeffrey@pixelindustries.com>
2018-11-23 10:10:31 +0100

    Use ChromeHeadless as testing environment

--- karma.conf.js
+++ karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
             'coverage',
             'mocha',
         ],
-        browsers: ['Chrome'],
+        browsers: ['ChromeHeadless'],
 
         coverageReporter: {
             type: 'in-memory',
