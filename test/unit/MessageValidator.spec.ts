dd95c0557426b7dc71d38d810a34702d2af06b74
Jeffrey Westerkamp <jeffrey@pixelindustries.com>
2018-11-25 14:13:14 +0100

    [Clean] MessageValidator test

--- test/unit/MessageValidator.spec.ts
+++ test/unit/MessageValidator.spec.ts
@@ -6,6 +6,11 @@ describe("[UNIT] MessageValidator", () => {
 
     const remoteURL = "https://remote-window.test";
     const remoteSource = window;
+    const makeMessageEvent = (data: any) => new MessageEvent("message", {
+        data,
+        origin: remoteURL,
+        source: remoteSource,
+    });
 
     let validator: MessageValidator;
 
@@ -14,30 +19,18 @@ describe("[UNIT] MessageValidator", () => {
         beforeEach(() => validator = new MessageValidator(window, remoteURL));
 
         it("Should validate good requests", () => {
-            const ev = new MessageEvent("message", {
-                data: makeValidRequest(),
-                origin: remoteURL,
-                source: remoteSource,
-            });
-            expect(validator.validate(ev as MessageEvent)).to.equal(true);
+            const ev = makeMessageEvent(makeValidRequest());
+            expect(validator.validate(ev)).to.equal(true);
         });
 
         it("Should validate good responses", () => {
-            const ev = new MessageEvent("message", {
-                data: makeValidResponse(),
-                origin: remoteURL,
-                source: remoteSource,
-            });
-            expect(validator.validate(ev as MessageEvent)).to.equal(true);
+            const ev = makeMessageEvent(makeValidResponse());
+            expect(validator.validate(ev)).to.equal(true);
         });
 
         it("Should validate good notifications", () => {
-            const ev = new MessageEvent("message", {
-                data: makeValidNotification(),
-                origin: remoteURL,
-                source: remoteSource,
-            });
-            expect(validator.validate(ev as MessageEvent)).to.equal(true);
+            const ev = makeMessageEvent(makeValidNotification());
+            expect(validator.validate(ev)).to.equal(true);
         });
 
         it("Should invalidate if given value is not a MessageEvent instance", () => {
@@ -45,42 +38,31 @@ describe("[UNIT] MessageValidator", () => {
                 data: makeValidRequest(),
                 origin: remoteURL,
                 source: remoteSource,
-            };
+            } as MessageEvent;
 
-            expect(validator.validate(ev as MessageEvent)).to.equal(false);
+            expect(validator.validate(ev)).to.equal(false);
         });
 
         it("Should invalidate if message.data does not have a valid type value", () => {
-
-            const ev = new MessageEvent("message", {
-                data: Object.assign(makeValidRequest(), {type: "NOT 'request' | 'response' | 'notification'"}),
-                origin: remoteURL,
-                source: remoteSource,
-            });
-
-            expect(validator.validate(ev as MessageEvent)).to.equal(false);
+            const ev = makeMessageEvent(
+                Object.assign(makeValidRequest(), {type: "NOT 'request' | 'response' | 'notification'"}),
+            );
+            expect(validator.validate(ev)).to.equal(false);
         });
 
         it("Should invalidate if message.data does not have a valid channel value", () => {
 
-            const ev = new MessageEvent("message", {
-                data: Object.assign(makeValidRequest(), {channel: undefined}),
-                origin: remoteURL,
-                source: remoteSource,
-            });
-
-            expect(validator.validate(ev as MessageEvent)).to.equal(false);
+            const ev = makeMessageEvent(
+                Object.assign(makeValidRequest(), {channel: undefined}),
+            );
+            expect(validator.validate(ev)).to.equal(false);
         });
 
         it("Should invalidate responses without a valid requestId value", () => {
-
-            const ev = new MessageEvent("message", {
-                data: Object.assign(makeValidResponse(), {requestId: undefined}),
-                origin: remoteURL,
-                source: remoteSource,
-            });
-
-            expect(validator.validate(ev as MessageEvent)).to.equal(false);
+            const ev = makeMessageEvent(
+                Object.assign(makeValidResponse(), {requestId: undefined}),
+            );
+            expect(validator.validate(ev)).to.equal(false);
         });
     });
 });
